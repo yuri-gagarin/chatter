c6b7c32ff512778221b959983578017f5cae8146
Yuriy Ivanov <ymivanov@icloud.com>
2018-08-16 20:45:57 +0200

    cookies and session added

--- router/room.js
+++ router/room.js
@@ -14,5 +14,21 @@ module.exports = function(router) {
             res.render('chatrooms.ejs');
             next();
         });
+    
+    ///session test 
+    router
+        .route('/setcolor')
+        .get(function(req, res, next) {
+            req.session.favColor = 'Red';
+            res.send('Setting favorite color!');
+            next();
+        });
+    
+    router 
+        .route('/getcolor')
+        .get(function(req, res, next) {
+            res.send("Favorite color is: " + (req.session.favColor === undefined ? "not Found" : req.session.favColor));
+            next();
+        });
 
 };
--- server.js
+++ server.js
@@ -3,10 +3,35 @@ const app = express();
 const path = require('path');
 const router = express.Router();
 const routes = require('./router/routes');
+const cookieParser = require('cookie-parser');
+const session = require('express-session');
+
+const env = process.env.NODE_ENV || 'development';
 
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(cookieParser());
+
+if (env === 'development') {
+    app.use(session({
+        secret: 'mydogalfa',
+        name: 'my-cookie',
+        resave: true,
+        saveUninitialized: true    
+    }));
+} else {
+    app.use(session({
+        secret: 'mydogalfa',
+        name: 'my-cookie',
+        resave: true,
+        saveUninitialized: true    
+    }));
+};
+
+
+
+
 
 routes(router);
 app.use(router);
@@ -14,5 +39,6 @@ app.use(router);
 const port  = process.env.PORT || 5000;
 app.listen(port, function() {
     console.log(`Server running at port ${port}`);
+    console.log(`Environment is ${env}`);
 });
 
